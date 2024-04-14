00d04dbc603c2e2400cc2a4de4c28977c97ad9a7
AndoAAA <tarverdyan070@gmail.com>
2024-04-14 14:35:43 +0400

    fix

--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -1,1 +1,48 @@
-import "style.scss";
+import "./style.css";
+import { useState } from "react";
+
+
+const Register = () => {
+
+    const onSubmit = (event) => {
+        event.preventDefault()
+    }
+
+    const [registerFormData, setRegisterFormData] = useState({
+        firstname: "",
+        lastname: "",
+        email: "",
+        password: ""
+    });
+
+    const { firstname, lastname, email, password } = registerFormData;
+
+    const inputChange = (event) => {
+        setRegisterFormData((prev) => ({
+            ...prev,
+            [event.target.name]: event.target.value
+        }));
+    };
+
+    const registerData = (click)=>{
+        console.log("hello");
+    }
+
+
+    return (
+        <section>
+            <h1>Registration Page</h1>
+            <form action="" onSubmit={onSubmit}>
+                <input type="text" placeholder="firstname" name="firstname" value={firstname} onChange={inputChange} />
+                <input type="text" placeholder="lastname" name="lastname" value={lastname} onChange={inputChange} />
+                <input type="email" placeholder="email" name="email" value={email} onChange={inputChange} />
+                <input type="password" placeholder="password" name="password" value={password} onChange={inputChange} />
+                <button onClick={registerData}>Register</button>
+            </form>
+        </section>
+
+
+    )
+}
+
+export default Register;
