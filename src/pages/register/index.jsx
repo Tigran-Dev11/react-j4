a953061ce84dcae6c334365bbd5e19f034fef7b3
Tigran <tigran@elitedevsquad.com>
2024-04-15 18:09:38 +0400

    fix

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -3,54 +3,80 @@ import css from "./style.module.scss";
 
 const Register = () => {
   // const validateEmail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  const [email, setEmail] = useState("");
-  const [text,setText] = useState("")
-  const change = (e) => {
-    setEmail(e.target.value);
-    setText(e.target.value)
+  const [inputData, setInputData] = useState({
+    userName: "",
+    email: ""
+  });
+
+  const { userName, email } = inputData;
+  const [text, setText] = useState("");
+  const onChangeInput = (e) => {
+    setInputData({
+      ...inputData,
+      [e.target.name]: e.target.value
+    });
   };
   const [message, setMessage] = useState("");
   const valids = (e) => {
-    e.preventDefault()
-    const regexEmail =  /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    if(regexEmail.test(email)){
-        setMessage("Email is Valid")
-    }else if(email === ''){
-        setMessage("Please Enter Email")
-    }else if(!regexEmail.test(email)){
-        setMessage("Email is Invalid")
-    }else{
-        setMessage("")
-    } if(username !== '' ){
-        setMessage("name is valid")
-        return
-    }if(username == ''){
-        setMessage("name is invalid")
-        return
+    e.preventDefault();
+    const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (regexEmail.test(email)) {
+      setMessage("Email is Valid");
+    } else if (email === "") {
+      setMessage("Please Enter Email");
+    } else if (!regexEmail.test(email)) {
+      setMessage("Email is Invalid");
+    } else {
+      setMessage("");
     }
-    
-  }
+
+    if (username !== "") {
+      setMessage("name is valid");
+      return;
+    }
+    if (username == "") {
+      setMessage("name is invalid");
+      return;
+    }
+  };
 
   return (
     <div className={css.root}>
       <form onSubmit={valids}>
         <h1>Register</h1>
-        <input type="text" name="username" placeholder="Check Firstname" />
-        <input type="text" name="Lastname" placeholder="Check Lastname" />
+        <input
+          type="text"
+          name="userName"
+          placeholder="Check Firstname"
+          onChange={onChangeInput}
+        />
+        <input
+          type="text"
+          name="lastName"
+          placeholder="Check Lastname"
+          onChange={onChangeInput}
+        />
         <input
           type="email"
           name="mail"
           placeholder="Check Email"
-          onChange={change}
+          onChange={onChangeInput}
         />
-        <input type="password" name="password" placeholder="Check Password" />
+        <input
+          type="password"
+          name="password"
+          placeholder="Check Password"
+          onChange={onChangeInput}
+        />
         <input
           type="password"
           name="copypassword"
-          placeholder="Check Copy Password"/>
-          <p>{message}</p>
+          placeholder="Check Copy Password"
+          onChange={onChangeInput}
+        />
+        <p>{message}</p>
         <button>Submit</button>
-
       </form>
     </div>
   );
