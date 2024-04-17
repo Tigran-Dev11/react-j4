87e8af9aa780f2f3628357c9f7f7186e7ec29983
AndoAAA <tarverdyan070@gmail.com>
2024-04-17 13:58:19 +0400

    fix

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,13 @@
-
-import Login from "../pages/login";
-import Register from "../pages/register";
+import Home from "../pages/home";
+import Login from "../pages/login/login";
+import Register from "../pages/register/register";
 import { ROUTES } from "../utils/constant";
 
 export const routes = () => [
+    {
+        Component: Home,
+        path: ROUTES.home
+    },
     {
         Component: Login,
         path: ROUTES.login
