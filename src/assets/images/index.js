efd4308ecdd20e2b519543e9f314042d18c09c32
AndoAAA <tarverdyan070@gmail.com>
2024-04-19 22:56:32 +0400

    lesson-6-Ando

---
+++ src/assets/images/index.js
@@ -0,0 +1,8 @@
+import showPasswordIcon from"./show-password-icon.png";
+import hidePasswordIcon from"./hide-password-icon.jpg";
+
+
+
+export const IMAGES = {
+    showPasswordIcon, hidePasswordIcon
+}
--- src/commons/input/index.jsx
+++ src/commons/input/index.jsx
@@ -1,9 +1,48 @@
+import { IMAGES } from "../../assets/images";
 import "./style.css";
+import { useState } from "react";
+
+const inputVariants = {
+    primary: "primary",
+    secondary: "secondary"
+};
 
 
-const Input = ({ type, placeholder, ...props }) => {
+const Input = ({ type, placeholder, onChange,
+    variant,
+    register,
+    error,
+    isPasswordMode,
+    ...rest }) => {
 
-    return <input placeholder={placeholder} type={type} {...props} />;
+    const [inputType, setInputType] = useState(type);
+
+    return (
+        <div>
+            <input
+                className={inputVariants[variant] || inputVariants.primary}
+                type={isPasswordMode ? inputType : type}
+                placeholder={placeholder}
+                onChange={onChange}
+                {...register}
+                {...rest}
+            />
+            {isPasswordMode && (
+                <>
+                    {inputType === "password" ? (
+                        <img src={IMAGES.showPasswordIcon} alt="icon"  onClick={() => {
+                            setInputType("text");
+                        }}/>
+                    ) : (
+                        <img src={IMAGES.hidePasswordIcon} alt="icon" onClick={() => {
+                            setInputType("password");
+                        }}/>
+                    )}
+                </>
+            )}
+            <p>{error?.message ?? null}</p>
+        </div>
+    )
 };
 
 export default Input;
