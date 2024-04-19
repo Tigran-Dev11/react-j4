c59c882c9911df6ee43850caa265a25cee8ce945
Armen <armeniritsyan995@gmail.com>
2024-04-19 18:55:12 +0400

    index register

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -13,3 +13,4 @@ const Register = () => {
     </div>
   );
 };
+export default Register;
--- src/validation/index.js
+++
@@ -1,6 +0,0 @@
-import { registerSchema } from "./register-schema.js";
-import { loginSchema } from "./login-schema.js";
-
-export const SCHEMA = {
-    registerSchema, loginSchema
-};
