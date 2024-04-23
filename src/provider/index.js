ff68ce272203ae201cbb42eeedf004afc2d02279
AndoAAA <tarverdyan070@gmail.com>
2024-04-23 20:04:20 +0400

    fix

---
+++ src/provider/index.js
@@ -0,0 +1,20 @@
+import { createContext, useState } from "react"
+
+
+
+const GlobalContext = createContext({});
+
+const GlobalProvider = ({ children }) => {
+
+    const [basketItem, setBasketItem] = useState([]);
+
+    let count = 10;
+
+    return (
+        <GlobalContext.Provider value={{ basketItem, setBasketItem, count }}>
+            {children}
+        </GlobalContext.Provider>
+    )
+};
+
+export { GlobalContext, GlobalProvider };
