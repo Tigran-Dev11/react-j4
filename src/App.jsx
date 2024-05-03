48154ceee5a4380d64bf43d44bab6c88ecd9e983
Tigran <tigran@elitedevsquad.com>
2024-05-03 19:05:26 +0400

    init vite react and use yarn

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 
-
 function App() {
-  
+
   return (
-    <div className="App">
-        <h1>hello j4</h1>
-    </div>
-  );
+    <>
+      
+    </>
+  )
 }
 
-export default App;
+export default App
