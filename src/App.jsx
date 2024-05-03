820fa0628a6cebd92c9290b81e0d36e1fe2da43c
Tigran <tigran@elitedevsquad.com>
2024-05-03 19:18:41 +0400

    setup .env

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { useEffect } from "react";
 
 function App() {
+   
+
+  useEffect(()=>{
+    const url = import.meta.env.VITE_API_URL;
+    console.log(url);
+  },[])
+
 
   return (
     <>
-      
+     
     </>
-  )
+  );
 }
 
-export default App
+export default App;
