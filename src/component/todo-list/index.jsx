2a5fd61ba046b412c7cb651ee8c2cf7d51621ced
AndoAAA <tarverdyan070@gmail.com>
2024-05-05 10:01:16 +0400

    fix

--- src/component/todo-list/index.jsx
+++ src/component/todo-list/index.jsx
@@ -1,16 +1,42 @@
-import { todos } from "../../utils/todos";
+import { useState } from "react";
 
-function TodoList (){
+function TodoList() {
+    const [todos, setTodos] = useState([]);
+    const [inputValue, setInputValue] = useState('');
 
-    return(
+    function handleChange(e) {
+        setInputValue(e.target.value)
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        setTodos([...todos, inputValue]);
+        setInputValue('');
+    }
+
+    function handleDelete(index) {
+        const newTodos = [...todos]
+        newTodos.splice(index, 1)
+        setTodos(newTodos)
+    }
+
+
+    return (
         <div>
-            {todos.map(todo=>{
-                <div key={todo.id}>
-                    <input type="checkbox" />
-                    <p>{todos.title}</p>
-                    <button>X</button>
-                </div>
-            })}
+            <h1>Todo List</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={inputValue} onChange={handleChange} />
+                <button>Add Todo</button>
+            </form>
+            <ul>
+                {todos.map((todo, index) => {
+                    <li key={index}>{todo}
+                        <button onClick={() => handleDelete(index)}>X</button>
+                    </li>
+
+                })}
+            </ul>
+
         </div>
     )
 };
