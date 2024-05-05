cc2f8074e79d54d2e66b9cdbfe80e82ecfb92878
AndoAAA <tarverdyan070@gmail.com>
2024-05-05 11:50:33 +0400

    fix

---
+++ src/component/todo-form/index.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import "./style.css";
+
+function TodoForm() {
+    const [text, setText] = useState("");
+    const [todos, setTodos] = useState([]);
+
+    function handleChange(e) {
+        setText(e.target.value)
+    };
+
+    function handleSubmit(e){
+        e.preventDefault();
+        setTodos([...todos, text]);
+        setText("");
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" value={text} onChange={handleChange} />
+            <button>Add Todo</button>
+        </form>
+    )
+};
+
+export default TodoForm;
