7ae31569e3dd4282f59b2280627614c693104f5d
Tigran <tigran@elitedevsquad.com>
2024-05-08 18:22:11 +0400

    fix

--- src/components/todo-form/todo-item/index.jsx
+++ src/components/todo-form/todo-item/index.jsx
@@ -1,33 +1,25 @@
-import {  initialTodos } from "../../../constant/index.js"; 
+import { initialTodos } from "../../../constant/index.js";
 import * as S from "./styled.js";
 import { useState } from "react";
 
 const TodoItem = ({ todo, deleteTodo }) => {
-
   const [todos, setTodos] = useState([initialTodos]);
 
-const changeStatus = (todos) =>{
+  const changeStatus = (todos) => {
     console.log(todos.name);
 
-    todo.status = true
-if(todo.status)
-     console.log();
-}
+    todo.status = true;
+    if (todo.status) console.log();
+  };
 
-
-    return (
-  
-      <S.TodoContainer>
-       <input type="checkbox"  onChange={() => changeStatus(todo.id)}/>
-        <S.TodoTask>{todo.name}</S.TodoTask>
-        <S.SubmitBtn onClick={()=>deleteTodo(todo.id)}>Delete</S.SubmitBtn>
-        <S.SubmitBtn>Edit</S.SubmitBtn>
-    
-        </S.TodoContainer>
-
-      
-    );
-
+  return (
+    <S.TodoContainer>
+      <input type="checkbox" onChange={() => changeStatus(todo.id)} />
+      <S.TodoTask>{todo.name}</S.TodoTask>
+      <S.SubmitBtn onClick={() => deleteTodo(todo.id)}>Delete</S.SubmitBtn>
+      <S.SubmitBtn>Edit</S.SubmitBtn>
+    </S.TodoContainer>
+  );
 };
 
 export default TodoItem;
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,25 +1,23 @@
-import * as S from "./styled"
+import * as S from "./styled";
 import TodoForm from "../../components/todo-form/todo-form";
 import TodoTask from "../../components/todo-form/todo-task";
 
 import { useState } from "react";
 
-import { initialTodos } from "../../constant/index"
+import { initialTodos } from "../../constant/index";
 
+const Home = () => {
+  const [todos, setTodos] = useState(initialTodos);
 
-const Home=()=>{
-
-  const [todos, setTodos] = useState([initialTodos]);
-
-    return(
-<S.HomeContainer>
-    <S.FormContainer>
+  return (
+    <S.HomeContainer>
+      <S.FormContainer>
         <S.TodoTitle>Todo Tasks</S.TodoTitle>
-        <TodoForm setTodos={setTodos}/>
-      <TodoTask todos={todos}  setTodos={setTodos} />
-    </S.FormContainer>
-</S.HomeContainer>
-    )
-}
+        <TodoForm setTodos={setTodos} />
+        <TodoTask todos={todos} setTodos={setTodos} />
+      </S.FormContainer>
+    </S.HomeContainer>
+  );
+};
 
 export default Home;
