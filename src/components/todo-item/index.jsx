7919183df97c30f516d6c7dc33bf6eae015dc6b3
Artyom <artyom15xachatryan@gmail.com>
2024-05-15 11:22:16 +0400

    lesson-14

---
+++ src/components/todo-item/index.jsx
@@ -0,0 +1,42 @@
+import { useDispatch, useSelector } from "react-redux";
+import { todoSelector } from "../../libs/redux/todo-slice/todo-selector";
+import { todoActions } from "../../libs/redux/todo-slice/todo-slice";
+
+
+const TodoItem = () => {
+
+    const { todo, todos } = useSelector(todoSelector);
+
+    const dispatch = useDispatch();
+
+    const deleteTodo = (id) => {
+        dispatch(todoActions.deleteTodo(id))
+    };
+
+    const isCompleted = (id) => {
+        dispatch(todoActions.isCompleted(id))
+    };
+
+    const editTodo = (id)=>{
+        dispatch(todoActions.editTodo())
+    }
+
+    return (
+        <div>
+            {todos.map((todo) => {
+                return (<div key={todo.id}>
+                    <input type="checkbox" onClick={() => isCompleted(todo.id)} />
+                    <p>{todo.title}</p>
+                    <div>
+                        <button onClick={() => editTodo(todo.id)}>Edit</button>
+                        <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+                    </div>
+                </div>
+                )
+            })}
+        </div>
+    )
+};
+
+
+export default TodoItem;
