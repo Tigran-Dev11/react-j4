096c7e3c1867c7fd429f5b5b75291f54f9b91f56
YesayanMartin <myesayan9@gnail.com>
2024-05-22 18:52:21 +0400

    fixed

--- src/components/home-sections/second-section/index.jsx
+++ src/components/home-sections/second-section/index.jsx
@@ -3,21 +3,24 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { productSelector } from "../../../redux/product-slice/selector";
 
+import { productActions } from "../../../redux/product-slice/product-slice";
+
 
 
 const SecondSection = () =>{
     const dispatch = useDispatch();
-    const { ptoducts } = useSelector(productSelector);
+    const { products } = useSelector(productSelector);
 
 
     useEffect(() => {
         dispatch(productActions.getProduct());
         
       }, []);
-console.log(products);
+
     return(
 <S.Container>
-    {ptoducts.map((item) => {
+  
+    {products.map((item) => {
         <h1>{item.title}</h1>
     })}
 
