34e4f2d4a3c9b5139eab4101bffa89f57f0ef87c
AndoAAA <tarverdyan070@gmail.com>
2024-05-24 18:45:04 +0400

    addBasket

--- src/components/product-item/index.jsx
+++ src/components/product-item/index.jsx
@@ -1,42 +1,38 @@
 import * as S from "./styled.js";
 import { productActions } from "../../libs/redux/product-slice/product-slice.js";
 import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { productSelector } from "../../libs/redux/product-slice/product-selector.js";
 
 const ProductItem = ({ product }) => {
+  const dispatch = useDispatch();
+  const { products } = useSelector(productSelector);
+  const [count, setCount] = useState(1);
 
-    const dispatch = useDispatch();
-    const [count, setCount] = useState(1);
+  const decrement = () => {
+    if (count > 1) setCount(count - 1);
+  };
 
-    const decrement = () => {
-        if (count > 1)
-            setCount(count - 1)
-    };
+  const increment = () => {
+    setCount(count + 1);
+  };
 
-    const increment = () => {
-        setCount(count + 1)
-    };
+  const addBasket = () => {
+    dispatch(productActions.addBasket());
+  };
 
-    const addBasket = () => {
-        dispatch(productActions.addBasket());
-    };
-
-
-    return (
-        <S.ProductsContainer>
-            <S.ProductTitle>{product.name}</S.ProductTitle>
-            <S.ProductImage src={product.img} alt="products" />
-            <S.OrderButtons>
-                <S.MinusButton onClick={decrement}>-</S.MinusButton>
-                <S.ProductQuantity>{count}</S.ProductQuantity>
-                <S.PlusButton onClick={increment}>+</S.PlusButton>
-            </S.OrderButtons>
-            <S.AddBasketButton onClick={addBasket}>Add Basket</S.AddBasketButton>
-        </S.ProductsContainer>
-    )
+  return (
+    <S.ProductsContainer>
+      <S.ProductTitle>{product.name}</S.ProductTitle>
+      <S.ProductImage src={product.img} alt="products" />
+      <S.OrderButtons>
+        <S.MinusButton onClick={decrement}>-</S.MinusButton>
+        <S.ProductQuantity>{count}</S.ProductQuantity>
+        <S.PlusButton onClick={increment}>+</S.PlusButton>
+      </S.OrderButtons>
+      <S.AddBasketButton onClick={addBasket}>Add Basket</S.AddBasketButton>
+    </S.ProductsContainer>
+  );
 };
 
-
 export default ProductItem;
-
-
