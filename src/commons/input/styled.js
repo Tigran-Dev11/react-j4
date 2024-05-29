549e66bd2c66c23e0cdccc796dce3a7a86ed449c
AndoAAA <tarverdyan070@gmail.com>
2024-05-29 14:32:40 +0400

    login-validation-styles

--- src/commons/input/styled.js
+++ src/commons/input/styled.js
@@ -2,5 +2,8 @@ import styled from "styled-components";
 
 
 export const Input = styled.input`
-
+    width: 400px;
+    height: 30px;
+    border-radius: 8px;
+    padding-left: 20px;
 `
--- src/pages/log-in/index.jsx
+++ src/pages/log-in/index.jsx
@@ -2,58 +2,47 @@ import * as S from "./styled.js";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { SCHEMA } from "../../validation";
-import { useNavigate } from "react-router-dom";
-import { ROUTES } from "../../utils/constant";
+
 import Input from "../../commons/input";
 
 const Login = () => {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors }
-    } = useForm({
-        resolver: yupResolver(SCHEMA.loginSchema)
-    });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(SCHEMA.loginSchema),
+  });
 
-    const navigate = useNavigate();
+  const onSubmit = (data) => {
+    localStorage.getItem("data", JSON.stringify(data));
+  };
 
-    const onSubmit = (data) => {
-        localStorage.getItem('data', JSON.stringify(data))
+  return (
+    <S.FormContainer className="container">
+      <S.PageTitle>Login page</S.PageTitle>
+      <S.LoginForm onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          type="email"
+          placeholder="Email *"
+          register={register("email")}
+          variant={"primary"}
+          error={errors?.email}
+        />
 
-        navigate(ROUTES.userpage)
-
-    };
-
-    return (
-        <div className="container">
-            <h1>Login page</h1>
-            <form action="" onSubmit={handleSubmit(onSubmit)}>
-
-                <Input
-                    type="email"
-                    placeholder="Email *"
-                    register={register("email")}
-                    variant={'primary'}
-                    error={errors?.email}
-
-                />
-
-                <Input
-                    type="password"
-                    placeholder="Password *"
-                    register={register("password")}
-                    error={errors?.password}
-                    variant={'primary'}
-                    isPasswordMode={true}
-                    className="loginPassword"
-                />
-
-
-                <button>Login</button>
-            </form>
-        </div>
-    );
+        <Input
+          type="password"
+          placeholder="Password *"
+          register={register("password")}
+          error={errors?.password}
+          variant={"primary"}
+          isPasswordMode={true}
+          className="loginPassword"
+        />
+        <S.LoginButton>Login</S.LoginButton>
+      </S.LoginForm>
+    </S.FormContainer>
+  );
 };
 
 export default Login;
-
