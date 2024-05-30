0e3b06fcc5c109c9b91079c5efe569795964ebf6
Armen <armeniritsyan995@gmail.com>
2024-05-30 17:30:19 +0400

    import

--- i18n.js
+++ i18n.js
@@ -1,18 +1,20 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import en from "./locales/en.json";
+import ru from "./locales/ru.json";
+import hy from "./locales/hy.json";
 
-i18n
-  .use(Backend)
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    fallbackLng: 'en',
-    debug: true,
-    interpolation: {
-      escapeValue: false,
-    }
-  });
+i18n.use(initReactI18next).init({
+  resources: {
+    en: { translation: en },
+    ru: { translation: ru },
+    hy: { translation: hy }
+  },
+  lng: "en", 
+  fallbackLng: "en",
+  interpolation: { escapeValue: false }
+});
 
 export default i18n;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,25 @@
+
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import * as S from './styled';
+import { useTranslation } from 'react-i18next'; 
 
 const Header = () => {
+  const { t } = useTranslation(); 
+
   return (
     <S.HeaderContainer>
       <S.Logo>GARRENT</S.Logo>
       <S.Navbar>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/about">About</NavLink>
-        <NavLink to="/booking">Booking</NavLink>
-        <NavLink to="/cars">Cars</NavLink>
-        <NavLink to="/contacts">Contacts</NavLink>
+        <NavLink to="/">{t('home')}</NavLink> 
+        <NavLink to="/about">{t('about')}</NavLink>
+        <NavLink to="/booking">{t('booking')}</NavLink>
+        <NavLink to="/cars">{t('cars')}</NavLink>
+        <NavLink to="/contacts">{t('contacts')}</NavLink>
       </S.Navbar>
       <S.AuthButtons>
-        <NavLink to="/login" className='btn'>Login</NavLink>
-        <NavLink to="/register" className='btn'>Sign Up</NavLink>
+        <NavLink to="/login" className='btn'>{t('login')}</NavLink>
+        <NavLink to="/register" className='btn'>{t('signup')}</NavLink>
       </S.AuthButtons>
     </S.HeaderContainer>
   );
