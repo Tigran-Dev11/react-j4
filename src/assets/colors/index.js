87da1f783b4ea7f7f16ff88a1bec576697262303
AndoAAA <tarverdyan070@gmail.com>
2024-06-01 15:34:38 +0400

    format code

--- src/assets/colors/index.js
+++ src/assets/colors/index.js
@@ -1,11 +1,10 @@
-
-
 export const COLORS = {
-
-    white: "white",
-    darkGray:" #808080",
-    green: "green",
-    gray: "#999999",
-    gray20: "#333333",
-    
+  white: "white",
+  darkGray: " #808080",
+  green: "green",
+  gray: "#999999",
+  gray20: "#333333",
 };
+
+
+
